2e23d488be6985badec66d8114d47f115d63384e
pawel-helon <150729481+pawel-helon@users.noreply.github.com>
2025-05-08 21:49:39 +0200

    Streaming database changes for slots

--- client/src/features/slots/Slots.tsx
+++ client/src/features/slots/Slots.tsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useGetWeekSlotsQuery, useGetWeekSlotsRecurringDatesQuery } from 'src/redux/api';
+import { api, useGetWeekSlotsQuery, useGetWeekSlotsRecurringDatesQuery } from 'src/redux/api';
 import { ThemeToggle } from 'src/components/ThemeToggle';
 import { Toasts } from 'src/components/Toasts';
 import { Breadcrumbs } from 'src/features/slots/Breadcrumbs';
@@ -10,6 +10,12 @@ import { destructureParams } from 'src/utils/destructureParams';
 import { useHandleBreakpoint } from 'src/hooks/useHandleBreakpoint';
 import { useHandleTheme } from 'src/hooks/useHandleTheme';
 import { getWeekDays } from 'src/utils/dates/getWeekDays';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from 'src/redux/store';
+import { io, Socket } from 'socket.io-client';
+import React from 'react';
+import { Slot } from 'src/types/slots';
+import { getDate } from 'src/utils/dates/getDate';
 
 export const Slots = () => {
   const employeeId = '071dcf39-b002-4588-95f4-dc7df1c2bc83';
@@ -45,7 +51,95 @@ export const Slots = () => {
           />
         </div>
         <Toasts />
+        <StreamReceiver
+          employeeId={employeeId}
+          weekDays={weekDays}
+        />
       </div>
     </div>
   )
+}
+
+const StreamReceiver = (props: {
+  employeeId: string,
+  weekDays: string[]
+}) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const socketRef: React.RefObject<Socket | null> = React.useRef(null);
+  
+  React.useEffect(() => {
+    socketRef.current = io(import.meta.env.VITE_STREAMING_URL);
+
+    const slotsUpdated = ( payload: {
+      eventAction: 'create' | 'update' | 'delete',
+      data: Slot
+    } ) => {
+      const start = props.weekDays[0];
+      const end = props.weekDays[props.weekDays.length - 1]
+      const slot = payload.data as Slot;
+      const slotDate = getDate(new Date(slot.startTime));
+      
+      if (payload.eventAction === 'create') {
+        if (props.weekDays.includes(slotDate)) {
+          /** Inserts slot into cached getWeekSlots data. */
+          dispatch(api.util.patchQueryData(
+            'getWeekSlots',
+            { employeeId: props.employeeId, start, end },
+            [
+              {
+                op: 'add',
+                path: ['byId', slot.id],
+                value: slot
+              },
+              {
+                op: 'add',
+                path: ['allIds', '-'],
+                value: slot.id
+              }
+            ]
+          ));
+        }
+      } else if (payload.eventAction === 'update') {
+
+        if (props.weekDays.includes(slotDate)) {
+            /** Updates slot in cached getWeekSlots data. */
+            dispatch(api.util.patchQueryData(
+              'getWeekSlots',
+              { employeeId: props.employeeId, start, end },
+              [
+                {
+                  op: 'replace',
+                  path: ['byId', slot.id],
+                  value: slot
+                } 
+              ]
+            ));
+        }
+      } else if (payload.eventAction === 'delete') {
+        /** Removes deleted slot from cached getWeekSlots data. */
+        dispatch(api.util.patchQueryData(
+          'getWeekSlots',
+          { employeeId: props.employeeId, start, end },
+          [
+            {
+              op: 'remove',
+              path: ['byId', slot.id],
+              value: slot
+            },
+            {
+              op: 'remove',
+              path: ['allIds', '-']
+            }
+          ]
+        ));
+      }
+    }
+    socketRef.current.on('slots', slotsUpdated);
+    
+    return () => {
+      socketRef.current?.off('slots', slotsUpdated);
+    };
+  },[dispatch, props])
+  
+  return null;
 }
